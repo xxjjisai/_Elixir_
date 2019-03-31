bf1ef33fcdfd150c6e4d2f13640dba6a686ba3b2
xxjjisai <1353231305@qq.com>
2019-03-31 08:53:14 +0800

    cool

--- scripts/scenes/Scene1/Scene_1.lua
+++ scripts/scenes/Scene1/Scene_1.lua
@@ -7,24 +7,8 @@ _G.Scene_1 =
 
     tbUI = 
     { 
-        -- { sUIType = 'ShapeButton', sUseName = 'btn_bg', tbProperty = {
-        --     sText = "";
-        --     x = 0, y = 0, w = 70, h = 640;
-        --     style = {
-        --         bBg = true,
-        --         bBorder = false,
-        --         sborderFill = "line",
-        --         sbgFill = "fill",
-        --         nFontSize = 182,
-        --         bgcolor = {0,0,0,0.3},
-        --         txtcolor = {0,0,0,0.3},
-        --         bordercolor = {0,0,0,0.3},
-        --         bHoverColor = {0,0,0,0.3},
-        --     };
-        -- }},
-
-        -- { sUIType = 'ShapeButton', sUseName = 'btn_1' },
-        -- { sUIType = 'ShapeTextInput', sUseName = 'input_1' },
+        { sUIType = 'ShapeButton', sUseName = 'btn_1' },
+        { sUIType = 'ShapeTextInput', sUseName = 'input_1' },
         -- { sUIType = 'ShapeList', sUseName = 'list_1' },
         -- { sUIType = 'ShapeGrid', sUseName = 'grid_1' },
     },
